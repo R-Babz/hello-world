89d4ab4967bc748df1d348b7eaca75eb61170a3d
R-Babz <reem.babz@gmail.com>
2017-09-12 10:18:38 -0500

    Python code_1
    -Create code to prompt user to enter area needed for calculation in square meters
    -Perform conversion calculation into acres, hectares, square feet and square inches

---
+++ Week1_Assignment_Babikir.Py.py
@@ -0,0 +1,17 @@
+user_input= ('Please enter the area in square meters:')
+print (user_input)
+15
+user_input = 15
+print(user_input)
+acres = user_input * 0.0002471054
+print(acres )
+hectares = user_input * 0.0001
+print (hectares)
+sqfeet = user_input * 10.76391
+print (sqfeet)
+sqinches = user_input * 1550.003
+print (sqinches)
+print(user_input, 'Square Meters(S) =', acres, 'Acres')
+print(user_input, 'Square Meters(S) =', hectares, 'Hectares')
+print (user_input, 'Square Meters(S) =', sqfeet, 'Square Feet')
+print (user_input, 'Square Meters(S) =', sqinches, 'Square Inches')
